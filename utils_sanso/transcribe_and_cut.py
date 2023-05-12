1cb5c5de8e4aba0c63f434b8bf9163b43fe1874e
JSansoP <61268003+JSansoP@users.noreply.github.com>
2023-05-12 19:28:28 +0200

    Transcribe and cut working

--- utils_sanso/transcribe_and_cut.py
+++ utils_sanso/transcribe_and_cut.py
@@ -6,9 +6,12 @@ import datetime
 import pandas as pd
 import ffmpeg
 from TTS.TTS.utils.text.cleaners import multilingual_cleaners
+import time
+import torch
 
 def main(args):
     #Check that the audio file exists
+    original_filename = args.filepath.split("\\")[-1]
     if not os.path.exists(args.filepath):
         raise Exception("The file {0} does not exist. Please check the path and try again.".format(args.filepath))
     
@@ -17,10 +20,13 @@ def main(args):
     out_folder = tmp_name + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     if not os.path.exists(out_folder):
         os.makedirs(os.path.join(out_folder, "wavs"))
-
-
+    
+    #Get audio file duration
+    duration = ffmpeg.probe(args.filepath)['format']['duration']
     #Transcribe the audio file using OpenAI Whisper
     print("Transcribing audio file...")
+    print("The duration of the file is {0} seconds, so expect at least a wait of that time.".format(duration))
+    time.sleep(1) #Wait 1 second to avoid bad allocation error
     subprocess.run(["whisper", args.filepath,"--model", "medium" ,"--language", args.language, "--output_format", "tsv", "--output_dir", out_folder])
 
     #Transform audio file into mono, 16 bit 22050 Hz wav file
@@ -29,13 +35,14 @@ def main(args):
 
     #Cut the audio file into clips using the transcription
     print("Cutting audio file into clips...")
-    cut_audio_and_generate_metadata(out_folder, transformed_audio_path)
+    cut_audio_and_generate_metadata(out_folder, transformed_audio_path, original_filename.replace(".wav", ".tsv"))
 
 
 
-def cut_audio_and_generate_metadata(out_folder: str, transformed_audio_path: str) -> None:
-    df = pd.read_csv(os.path.join(out_folder, "transcription.tsv"), sep="\t", header=0)
-    with open(os.path.join(out_folder, "metadata.txt"), "r") as f:
+
+def cut_audio_and_generate_metadata(out_folder: str, transformed_audio_path: str, transcription_file) -> None:
+    df = pd.read_csv(os.path.join(out_folder, transcription_file), sep="\t", header=0)
+    with open(os.path.join(out_folder, "metadata.txt"), "w", encoding='utf8') as f:
         for index, row in df.iterrows():
             start_ms = row["start"]
             end_ms = row["end"]
@@ -43,7 +50,7 @@ def cut_audio_and_generate_metadata(out_folder: str, transformed_audio_path: str
             stream = ffmpeg.output(stream, os.path.join(out_folder, "wavs", str(index+1) + ".wav"), ss=start_ms/1000, to=end_ms/1000)
             ffmpeg.run(stream)
             cleaned_text = multilingual_cleaners(row["text"])
-            f.write('/content/tacotron2/wavs/{0}.wav|{2}\n'.format(str(index+1), cleaned_text))
+            f.write('/content/tacotron2/wavs/{0}.wav|{1}\n'.format(str(index+1), cleaned_text))
 
 
 def transform_audio_file(filepath: str, out_folder: str) -> str:
@@ -53,6 +60,14 @@ def transform_audio_file(filepath: str, out_folder: str) -> str:
     stream = ffmpeg.output(stream, transformed_audio_path, ac=1, ar=22050, acodec="pcm_s16le")
     ffmpeg.run(stream)
     return transformed_audio_path
+
+
+def force_cudnn_initialization():
+    s = 32
+    dev = torch.device('cuda')
+    torch.nn.functional.conv2d(torch.zeros(s, s, s, s, device=dev), torch.zeros(s, s, s, s, device=dev))
+
+
 
 if __name__ == "__main__":
     argparse = argparse.ArgumentParser(description='Script to transcribe and cut long audio files into short clips using OpenAI Whisper and ffmpeg.')
@@ -65,4 +80,5 @@ if __name__ == "__main__":
     filename = args.filepath.split("\\")[-1]
     if re.search(r'[^A-Za-z0-9_\.]+', filename):
         raise Exception("File name contains special characters or spaces. Please rename the file and try again.")
+    force_cudnn_initialization() #Trick to avoid CUDNN_STATUS_NOT_INITIALIZED error when running Whisper
     main(args)
