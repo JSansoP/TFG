60926386b8ca2b29bd7d4614f5cc9f793eb0160b
JSansoP <61268003+JSansoP@users.noreply.github.com>
2023-06-07 19:29:26 +0200

    More GUI Work, refactoring packages

--- gui/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def project_exists(project_name):
-    return os.path.isdir(os.path.join("projects", project_name))
-
-def create_project(project_name):
-    print("Creating project: {}".format(project_name))
